9672db96e1df3b27e04f04f91926d429969fe9c6
ViktorViktorovitsh <90757994+ViktorViktorovitsh@users.noreply.github.com>
2021-12-20 09:08:35 +0400

    Update data.py

--- imports/datapack/data.py
+++ imports/datapack/data.py
@@ -1,1 +1,17 @@
+print('start data')
+documents = [
+    {"type": "diplom", "number": "112233", "name": "Илон Маск"},
+    {"type": "medical_book", "number": "1234", "name": "Илон Маск"},
+    {"type": "insurance", "number": "100", "name": "Илон Маск"},
+    {"type": "diplom", "number": "333555", "name": "Билл Гейтс"},
+    {"type": "medical_book", "number": "2222", "name": "Билл Гейтс"},
+    {"type": "diplom", "number": "778899", "name": "Линус Торвальдс"},
+    {"type": "insurance", "number": "333", "name": "Линус Торвальдс"},
+]
 
+directories = {
+    '1': ['112233', '333555', '778899'],
+    '2': ['1234', '2222'],
+    '3': ['100', '333']
+}
+print('end data')
