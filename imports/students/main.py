a8e43328dde335ba14279b86e05c18ef30512893
ViktorViktorovitsh <90757994+ViktorViktorovitsh@users.noreply.github.com>
2021-12-27 12:23:15 +0400

    Update main.py

--- imports/students/main.py
+++ imports/students/main.py
@@ -1,1 +1,17 @@
+# v1
+# from apps.students import get_users
+# from apps.db.rating import get_middle, max_rating
 
+
+# if __name__ == '__main__':
+#     get_users()
+#     get_middle()
+
+
+#v2
+from apps import students
+from apps.db import rating
+
+if __name__ == '__main__':
+    students.get_users()
+    rating.get_middle()
